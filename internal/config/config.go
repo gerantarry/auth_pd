d109994d1ff687c13e1c80e748fd4060f79833d3
Anton <geraskin.antony@yandex.ru>
2022-08-11 23:46:05 +0700

    config.yaml config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,4 +1,16 @@
 package config
 
 type Config struct {
+	IsDebug *bool `yaml:"is_debug"`
+	Listen  struct {
+		BindIp string `yaml:"bind_ip"`
+		Port   string `yaml:"port"`
+	} `yaml:"listen"`
+	Database struct {
+		Scheme   string `yaml:"scheme"`
+		Login    string `yaml:"login"`
+		Password string `yaml:"password"`
+		BindIp   string `yaml:"bind_ip"`
+		port     string `yaml:"port"`
+	}
 }
