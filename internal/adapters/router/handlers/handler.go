67ae72d89a7f5cbd4874e6f1c4cdc0a21406a483
Anton <geraskin.antony@yandex.ru>
2022-08-21 10:20:51 +0700

    handler struct + constructor

--- internal/adapters/router/handlers/handler.go
+++ internal/adapters/router/handlers/handler.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"auth_pd/internal/adapters/db/mysql"
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	storage *mysql.Storage
+}
+
+func NewHandler(stg *mysql.Storage) *handler {
+	return &handler{storage: stg}
+}
+
+func Register(c *gin.Context) {
+
+}
