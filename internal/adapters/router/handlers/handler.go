b82d3dbb322186dce4dac89568a90439f2931456
Anton <geraskin.antony@yandex.ru>
2022-08-22 18:46:34 +0700

    register handler changed

--- internal/adapters/router/handlers/handler.go
+++ internal/adapters/router/handlers/handler.go
@@ -4,7 +4,9 @@ import (
 	"auth_pd/internal/adapters"
 	"auth_pd/internal/adapters/db/mysql"
 	"auth_pd/internal/domain/dto"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"net/http"
 )
 
@@ -22,6 +24,12 @@ func NewHandler(stg *mysql.Storage, l *adapters.AppLogger) *Handler {
 
 func (h *Handler) Register(c *gin.Context) {
 	var regForm dto.RegisterForm
-	c.BindJSON(&regForm)
+	h.logger.Debug("Получен запрос. Начинаем биндить тело")
+	if err := c.MustBindWith(&regForm, binding.JSON); err != nil {
+		h.logger.Errorf("Не удалось разобрать запрос. Причина - %v", err.Error())
+		return
+	}
+	fmt.Println(regForm)
 	c.JSON(http.StatusOK, "register test OK")
+
 }
